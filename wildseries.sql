973f62329c973c106eee260666d8a7e0c88ecea5
ComteDeX <comtedex@gmail.com>
2019-12-31 18:48:11 +0100

    Quête n°15

--- wildseries.sql
+++ wildseries.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.27, for osx10.12 (x86_64)
 --
--- Host: localhost    Database: wild-series_GUENEL_Xavier
+-- Host: localhost    Database: wildseries
 -- ------------------------------------------------------
 -- Server version	5.7.27
 
@@ -16,6 +16,58 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `actor`
+--
+
+DROP TABLE IF EXISTS `actor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `actor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actor`
+--
+
+LOCK TABLES `actor` WRITE;
+/*!40000 ALTER TABLE `actor` DISABLE KEYS */;
+INSERT INTO `actor` VALUES (1,'Andrew Lincoln'),(2,'Norman Reedus'),(3,'Lauren Cohan'),(4,'Danai Gurira'),(7,'Louis Hofmann'),(8,'Andreas Pietschmann'),(9,'Oliver Masucci'),(10,'Lisa Vicari');
+/*!40000 ALTER TABLE `actor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `actor_program`
+--
+
+DROP TABLE IF EXISTS `actor_program`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `actor_program` (
+  `actor_id` int(11) NOT NULL,
+  `program_id` int(11) NOT NULL,
+  PRIMARY KEY (`actor_id`,`program_id`),
+  KEY `IDX_B01827EE10DAF24A` (`actor_id`),
+  KEY `IDX_B01827EE3EB8070A` (`program_id`),
+  CONSTRAINT `FK_B01827EE10DAF24A` FOREIGN KEY (`actor_id`) REFERENCES `actor` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_B01827EE3EB8070A` FOREIGN KEY (`program_id`) REFERENCES `program` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actor_program`
+--
+
+LOCK TABLES `actor_program` WRITE;
+/*!40000 ALTER TABLE `actor_program` DISABLE KEYS */;
+INSERT INTO `actor_program` VALUES (1,1),(1,4),(2,1),(3,1),(4,1),(7,9),(8,9),(9,9),(10,9);
+/*!40000 ALTER TABLE `actor_program` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `category`
 --
 
@@ -24,9 +76,9 @@ DROP TABLE IF EXISTS `category`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,7 +87,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'Horreur');
+INSERT INTO `category` VALUES (1,'Horreur'),(2,'Love story'),(3,'Aventure'),(4,'Action');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,11 +103,11 @@ CREATE TABLE `episode` (
   `season_id` int(11) NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `number` int(11) NOT NULL,
-  `synopsis` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sysnopsis` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_DDAA1CDA4EC001D1` (`season_id`),
   CONSTRAINT `FK_DDAA1CDA4EC001D1` FOREIGN KEY (`season_id`) REFERENCES `season` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,7 +116,7 @@ CREATE TABLE `episode` (
 
 LOCK TABLES `episode` WRITE;
 /*!40000 ALTER TABLE `episode` DISABLE KEYS */;
-INSERT INTO `episode` VALUES (1,1,'Days Gone By',1,'Deputy Sheriff Rick Grimes awakens from a coma, and searches for his family in a world ravaged by the undead.'),(2,1,'Guts',2,'In Atlanta, Rick is rescued by a group of survivors, but they soon find themselves trapped inside a department store surrounded by walkers.'),(3,1,'Tell It to the Frogs',3,'Rick is reunited with Lori and Carl but soon decides - along with some of the other survivors - to return to the rooftop and rescue Merle. Meanwhile, tensions run high between the other survivors at the camp.'),(4,1,'Vatos',4,'Rick, Glenn, Daryl and T-Dog come across a group of seemingly hostile survivors whilst searching for Merle. Back at camp, Jim begins behaving erratically.'),(5,1,'Wildfire',5,'After the attack on the camp, Rick leads the survivors to the C.D.C., in the hope that they can cure an infected Jim.'),(6,1,'TS-19',1,'The survivors gain access to the C.D.C. in the hope of a safe haven.');
+INSERT INTO `episode` VALUES (1,1,'Days Gone By',1,'Deputy Sheriff Rick Grimes awakens from a coma, and searches for his family in a world ravaged by the undead.\r\nDescription en anglais, si vous ne comprenez pas, allez voir sur Wikipedia'),(2,1,'Guts',2,'In Atlanta, Rick is rescued by a group of survivors, but they soon find themselves trapped inside a department store surrounded by walkers.'),(3,1,'Tell It to the Frogs',3,'Rick is reunited with Lori and Carl but soon decides - along with some of the other survivors - to return to the rooftop and rescue Merle. Meanwhile, tensions run high between the other survivors at the camp.'),(4,1,'Vatos',4,'Rick, Glenn, Daryl and T-Dog come across a group of seemingly hostile survivors whilst searching for Merle. Back at camp, Jim begins behaving erratically.'),(5,1,'Wildfire',5,'After the attack on the camp, Rick leads the survivors to the C.D.C., in the hope that they can cure an infected Jim.'),(6,1,'TS-19',2,'The survivors gain access to the C.D.C. in the hope of a safe haven.'),(7,29,'Sonnie’s Edge',1,'Une femme nommée Sonnie contrôle à distance un monstre créé par ingénierie génétique dans des combats de gladiateurs souterrains.'),(8,29,'Three Robots',2,'Après la destruction de l’humanité, trois robots visitent une ville abandonnée.'),(9,29,'The Witness',3,'Une femme assiste à un meurtre dans l’immeuble situé en face de sa chambre d’hôtel. Une poursuite s’engage quand elle croise le regard du meurtrier.'),(10,29,'Suits',4,'Une petite communauté de fermiers pilote des mechas pour défendre leurs terres contre une invasion d’aliens insectoïdes.'),(11,29,'Sucker of Souls',5,'Réveillé par une expédition archéologique, un démon assoiffé de sang affronte un groupe de mercenaires.'),(12,29,'When The Yogurt Took Over',6,'Le yaourt devient conscient et prend le contrôle de la société.');
 /*!40000 ALTER TABLE `episode` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +140,7 @@ CREATE TABLE `migration_versions` (
 
 LOCK TABLES `migration_versions` WRITE;
 /*!40000 ALTER TABLE `migration_versions` DISABLE KEYS */;
-INSERT INTO `migration_versions` VALUES ('20191124181014','2019-11-24 18:11:08'),('20191125105747','2019-11-25 10:58:52'),('20191126102538','2019-11-26 10:34:03');
+INSERT INTO `migration_versions` VALUES ('20191228143215','2019-12-28 14:32:26'),('20191228143543','2019-12-28 14:35:54'),('20191228151732','2019-12-28 15:17:38'),('20191228220140','2019-12-28 22:01:49'),('20191230211919','2019-12-30 21:19:30');
 /*!40000 ALTER TABLE `migration_versions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -101,19 +153,14 @@ DROP TABLE IF EXISTS `program`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `program` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `category_id` int(11) NOT NULL,
-  `season_id` int(11) NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `summary` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `poster` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `country` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `year` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_92ED778412469DE2` (`category_id`),
-  KEY `IDX_92ED77844EC001D1` (`season_id`),
-  CONSTRAINT `FK_92ED778412469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
-  CONSTRAINT `FK_92ED77844EC001D1` FOREIGN KEY (`season_id`) REFERENCES `season` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  CONSTRAINT `FK_92ED778412469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -122,6 +169,7 @@ CREATE TABLE `program` (
 
 LOCK TABLES `program` WRITE;
 /*!40000 ALTER TABLE `program` DISABLE KEYS */;
+INSERT INTO `program` VALUES (1,'Walking Dead','Le policier Rick Grimes se réveille après un long coma. Il découvre avec effarement que le monde, ravagé par une épidémie, est envahi par les morts-vivants. Série bien dégueux avec des Zombies.','https://m.media-amazon.com/images/M/MV5BZmFlMTA0MmUtNWVmOC00ZmE1LWFmMDYtZTJhYjJhNGVjYTU5XkEyXkFqcGdeQXVyMTAzMDM4MjM0._V1_.jpg',1),(2,'The Haunting Of Hill House','Plusieurs frères et sœurs qui, enfants, ont grandi dans la demeure qui allait devenir la maison hantée la plus célèbre des États-Unis, sont contraints de se réunir pour finalement affronter les fantômes de leur passé.','https://m.media-amazon.com/images/M/MV5BMTU4NzA4MDEwNF5BMl5BanBnXkFtZTgwMTQxODYzNjM@._V1_SY1000_CR0,0,674,1000_AL_.jpg',1),(3,'Penny Dreadful','Dans le Londres ancien, Vanessa Ives, une jeune femme puissante aux pouvoirs hypnotiques, allie ses forces à celles de Ethan, un garçon rebelle et violent aux allures de cowboy, et de Sir Malcolm, un vieil homme riche aux ressources inépuisables. Ensemble, ils combattent un ennemi inconnu, presque invisible, qui ne semble pas humain et qui massacre la population.\r\nSérie à la limite du Steampunk.','https://m.media-amazon.com/images/M/MV5BNmE5MDE0ZmMtY2I5Mi00Y2RjLWJlYjMtODkxODQ5OWY1ODdkXkEyXkFqcGdeQXVyNjU2NjA5NjM@._V1_SY1000_CR0,0,695,1000_AL_.jpg',1),(4,'Fear The Walking Dead','La série se déroule au tout début de l épidémie relatée dans la série mère The Walking Dead et se passe dans la ville de Los Angeles, et non à Atlanta. Madison est conseillère dans un lycée de Los Angeles. Depuis la mort de son mari, elle élève seule ses deux enfants : Alicia, excellente élève qui découvre les premiers émois amoureux, et son grand frère Nick qui a quitté la fac et a sombré dans la drogue.','https://m.media-amazon.com/images/M/MV5BYWNmY2Y1NTgtYTExMS00NGUxLWIxYWQtMjU4MjNkZjZlZjQ3XkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_SY1000_CR0,0,666,1000_AL_.jpg',1),(5,'American Horror Story','A chaque saison, son histoire. American Horror Story nous embarque dans des récits à la fois poignants et cauchemardesques, mêlant la peur, le gore et le politiquement correct.','https://m.media-amazon.com/images/M/MV5BODZlYzc2ODYtYmQyZS00ZTM4LTk4ZDQtMTMyZDdhMDgzZTU0XkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_SY1000_CR0,0,666,1000_AL_.jpg',1),(6,'Love Death And Robots','Un yaourt susceptible, des soldats lycanthropes, des robots déchaînés, des monstres-poubelles, des chasseurs de primes cyborgs, des araignées extraterrestres et des démons assoiffés de sang : tout ce beau monde est réuni dans 18 courts métrages animés déconseillés aux âmes sensibles.','https://m.media-amazon.com/images/M/MV5BMTc1MjIyNDI3Nl5BMl5BanBnXkFtZTgwMjQ1OTI0NzM@._V1_SY1000_CR0,0,674,1000_AL_.jpg',1),(9,'Dark','Description Lorsque deux enfants disparaissent dans une petite ville allemande, son passé tragique refait surface. Quatre familles à la recherche des enfants vont voir leur routine bouleversée et les secrets de chacun vont être mis en lumière.','https://m.media-amazon.com/images/I/B1cB5pEqr2S._SS500_.jpg',1);
 /*!40000 ALTER TABLE `program` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -134,11 +182,14 @@ DROP TABLE IF EXISTS `season`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `season` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `program_id` int(11) NOT NULL,
+  `number` int(11) NOT NULL,
   `year` int(11) NOT NULL,
   `description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `number` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  PRIMARY KEY (`id`),
+  KEY `IDX_F0E45BA93EB8070A` (`program_id`),
+  CONSTRAINT `FK_F0E45BA93EB8070A` FOREIGN KEY (`program_id`) REFERENCES `program` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -147,7 +198,7 @@ CREATE TABLE `season` (
 
 LOCK TABLES `season` WRITE;
 /*!40000 ALTER TABLE `season` DISABLE KEYS */;
-INSERT INTO `season` VALUES (1,2010,'1ère saison de The Walking Dead',0),(2,2011,'2nde saison de The Walking Dead',0),(3,2011,'2nde saison de The Walking Dead',0),(4,2012,'3e saison de The Walking Dead',0),(5,2013,'4e saison de The Walking Dead',0),(6,2014,'5e saison de The Walking Dead',0),(7,2015,'6e saison de The Walking Dead',0),(8,2016,'7e saison de The Walking Dead',0),(9,2017,'8e saison de The Walking Dead',0),(10,2018,'9e saison de The Walking Dead',0),(11,2019,'10e saison de The Walking Dead',0);
+INSERT INTO `season` VALUES (1,1,1,2010,'1ère saison de Walking Dead'),(2,1,2,2011,'2e saison de Walking Dead'),(3,1,3,2012,'3e saison de Walking Dead'),(4,1,4,2013,'4e saison de Walking Dead'),(5,1,5,2014,'5e saison de Walking Dead'),(6,1,6,2015,'6e saison de Walking Dead'),(7,1,7,2016,'7e saison de Walking Dead'),(8,1,8,2017,'8e saison de Walking Dead'),(9,1,9,2018,'9e saison de Walking Dead'),(10,1,10,2019,'10e saison de Walking Dead'),(11,2,1,2018,'1ère saison de The Haunting Of Hill House'),(12,3,1,2014,'1ère saison de Penny Dreadful'),(13,3,2,2015,'2e saison de Penny Dreadful'),(14,3,3,2016,'3e saison de Penny Dreadful'),(15,4,1,2015,'1ère saison de Fear the Walking Dead'),(16,4,2,2016,'2e saison de Fear the Walking Dead'),(17,4,3,2017,'3e saison de Fear the Walking Dead'),(18,4,4,2018,'4e saison de Fear the Walking Dead'),(19,4,5,2019,'5e saison de Fear the Walking Dead'),(20,5,1,2011,'1ère saison de American Horror Story'),(21,5,2,2012,'2e saison de American Horror Story'),(22,5,3,2013,'3e saison de American Horror Story'),(23,5,4,2014,'4e saison de American Horror Story'),(24,5,5,2015,'5e saison de American Horror Story'),(25,5,6,2016,'6e saison de American Horror Story'),(26,5,7,2017,'7e saison de American Horror Story'),(27,5,8,2018,'8e saison de American Horror Story'),(28,5,9,2019,'9e saison de American Horror Story'),(29,6,1,2019,'Saison unique de Love Death and Robots');
 /*!40000 ALTER TABLE `season` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -160,4 +211,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-12-29 11:57:47
+-- Dump completed on 2019-12-31 17:16:28
